4e0e053416036410b227b7ff6ed55148ff65ddfc
GusEs <gus_24@hotmail.com.ar>
2021-08-22 17:09:54 -0300

    fix features

--- src/test/TestCliente.java
+++ src/test/TestCliente.java
@@ -8,7 +8,7 @@ public class TestCliente {
         Cliente c1 = new Cliente("Juan", 'm', 27, "Entre Rios 2500", new Date(), true);       
         System.out.println(c1.toString());
         
-        Cliente c2 = new Cliente("Juan", 'm', 27, "Entre Rios 2500", new Date(), true);       
+        Cliente c2 = new Cliente("Juana", 'f', 22, "Entre Rios 2500", new Date(), false);       
         System.out.println(c2.toString());
     }
 }
